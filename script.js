04eda1170a736170eb20ff9d96baf709aae1f436
d4nmc <danielmcgee.work@gmail.com>
2021-07-13 09:40:46 +0100

    first commit playing with JS, nothing works :D

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Definitely NOT A Scam Page");
+
+let str1 = "Welcome to Freebies for Idiots";
+
+console.log("First Name");
+
+console.info("Second Name");
+
+console.warn("Born in?");
+
+console.error("Shoe Size");
+
+console.log("This is now the end of the exercises.", "color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+
